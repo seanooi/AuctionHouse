cd8dd35e71528b2ceaac3a5971df3bc5e436ccfa
Dan Hamlin <danhamlin@gmail.com>
2011-04-03 14:54:02 -0500

    auction detail view is now working, still working on bidding and adding new auctions.

--- src/main/java/org/cs300/auctionhouse/controllers/AuctionController.java
+++ src/main/java/org/cs300/auctionhouse/controllers/AuctionController.java
@@ -1,12 +1,14 @@
 package org.cs300.auctionhouse.controllers;
 
 import org.cs300.auctionhouse.domain.Auction;
+import org.cs300.auctionhouse.domain.Bid;
 import org.cs300.auctionhouse.services.Services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,22 +19,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuctionController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	//Spring will inject the services here we hope
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
+	// Spring will inject the services here we hope
 	@Autowired
 	protected Services services;
 
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value="/auction", method=RequestMethod.GET)
-	public String home(@RequestParam("id")int id, Model model) {
+	@RequestMapping(value = "/auction", method = RequestMethod.GET)
+	public String auction(@RequestParam("id") int id, Model model) {
 		logger.info("auction get request");
 		Auction auction = services.getAuctionByID(id);
+		Bid bid = new Bid();
 		model.addAttribute("auction", auction);
-		return "home";
+		model.addAttribute("bid", bid);
+		return "auction/auction";
 	}
-	
-}
 
+	@RequestMapping(value = "/auction", method = RequestMethod.POST)
+	public String bid(@ModelAttribute("auction") Auction auction,
+			@ModelAttribute("bid") Bid bid, Model model) {
+		logger.info("auction post request");
+		// FIXME:need to get currently logged in user and place bid if:
+		// current user is authenticated and != to auction owner
+		return "redirect:auction/bidsuccess";
+	}
+
+	@RequestMapping(value = "/auction/add", method = RequestMethod.GET)
+	public String addAuctionForm(Model model) {
+		// FIXME:form for listing new auction
+		return "auction/add";
+	}
+
+	@RequestMapping(value = "/auction/add", method = RequestMethod.POST)
+	public String addAuctionSubmit(Model model) {
+		// FIXME:persist new form to the db
+		return "redirect:auction/addsuccess";
+	}
+}
